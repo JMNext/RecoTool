28bf612884205977f1cf744f19caeabd16da1721
Yuri Mayorov <49004740+JMNext@users.noreply.github.com>
2024-02-14 16:45:28 +0300

    Update __version__.py

--- src/officialeye/__version__.py
+++ src/officialeye/__version__.py
@@ -1,17 +1,18 @@
-__title__ = "officialeye"
+__title__ = "recotool"
 __description__ = "An AI-powered generic document-analysis tool."
-__url__ = "https://officialeye.zerobone.net"
+__url__ = "https://github.com/JMNext/RecoTool"
 __version__ = "1.2.2"
 __author__ = "Alexander Mayorov"
 __author_email__ = "zb@zerobone.net"
 __license__ = "GPL-3.0"
 __copyright__ = "Copyright 2024 Alexander Mayorov (zb@zerobone.net, zerobone.net)"
-__github_url__ = "github.com/ZeroBone/OfficialEye"
-__github_full_url__ = "https://github.com/ZeroBone/OfficialEye"
-__ascii_logo__ = """   ____  _________      _       __   ______         
-  / __ \\/ __/ __(_)____(_)___ _/ /  / ____/_  _____ 
- / / / / /_/ /_/ / ___/ / __ `/ /  / __/ / / / / _ \\
-/ /_/ / __/ __/ / /__/ / /_/ / /  / /___/ /_/ /  __/
-\\____/_/ /_/ /_/\\___/_/\\__,_/_/  /_____/\\__, /\\___/ 
-                                       /____/                   
+__github_url__ = "github.com/JMNext/RecoTool"
+__github_full_url__ = "https://github.com/JMNext/RecoTool"
+__ascii_logo__ = """ ________  _______   ________  ________  _________  ________  ________  ___
+|\\   __  \\|\\  ___ \\ |\\   ____\\|\\   __  \\|\\___   ___\\\\   __  \\|\\   __  \\|\\  \\
+\\ \\  \\|\\  \\ \\   __/|\\ \\  \\___|\\ \\  \\|\\  \\|___ \\  \\_\\ \\  \\|\\  \\ \\  \\|\\  \\ \\  \\
+ \\ \\   _  _\\ \\  \\_|/_\\ \\  \\    \\ \\  \\\\\\  \\   \\ \\  \\ \\ \\  \\\\\\  \\ \\  \\\\\\  \\ \\  \\
+  \\ \\  \\\\  \\\\ \\  \\_|\\ \\ \\  \\____\\ \\  \\\\\\  \\   \\ \\  \\ \\ \\  \\\\\\  \\ \\  \\\\\\  \\ \\  \\____
+   \\ \\__\\\\ _\\\\ \\_______\\ \\_______\\ \\_______\\   \\ \\__\\ \\ \\_______\\ \\_______\\ \\_______\\
+    \\|__|\\|__|\\|_______|\\|_______|\\|_______|    \\|__|  \\|_______|\\|_______|\\|_______|
 """
